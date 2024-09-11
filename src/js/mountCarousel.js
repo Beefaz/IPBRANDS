c2d6c5b6f379592cd32b6acb37ca3f6e0af87b12
Beefaz <justinas.geryba@gmail.com>
2024-09-11 08:28:50 +0200

    potential fix for height of carousel not rendering properly in production

--- src/js/mountCarousel.js
+++ src/js/mountCarousel.js
@@ -1,7 +1,9 @@
 //mounts carousel and changes location of buttons in order to match style
 
 export function mountCarousel(className = 'main-carousel') {
-  new Flickity(`.${className}`);
+  new Flickity(`.${className}`, {
+    lazyLoad: false,
+  });
   const carousel = document.getElementsByClassName(className)[0];
   const controls = carousel.querySelectorAll(`.flickity-prev-next-button.previous, .flickity-page-dots, .flickity-prev-next-button.next`);
   const controlsContainer = document.createElement('div');
